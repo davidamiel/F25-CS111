f888f918eac089897b73b1bb2208c9a973f46694
David Amiel <david.amiel@rutgers.edu>
2025-10-24 07:29:18 -0400

    W8L2
    Week 8 Class Code

---
+++ 8. Functions/CircleFunctions.java
@@ -0,0 +1,24 @@
+public class CircleFunctions {
+
+    // Function 1 - getRadius
+    // Gets the radius of a circle from the user as an integer from the command line.
+
+    public static int getRadius() {
+        StdOut.println("Please enter a radius: ");
+        int rad = StdIn.readInt();
+        return rad;
+    }
+
+    // Function 2 - calculateArea
+    // Takes an integer radius and returns the area of that circle
+    public static double calculateArea(int r) {
+        double area = Math.PI * Math.pow(r, 2);
+        return area;
+    }
+
+    // Function 3 - calculateCircumference
+    public static double calculateCircuference(int rad) {
+        return (2 * Math.PI * rad);
+    }
+    
+}
---
+++ 8. Functions/CircleOperations.java
@@ -0,0 +1,10 @@
+public class CircleOperations {
+
+    public static void main(String[] args) {
+
+        int rad = CircleFunctions.getRadius();
+        System.out.println(CircleFunctions.calculateArea(rad));
+        
+    }
+    
+}
